7226d9163881d845865b18a3ded23757cbbc0751
Prasadhire <hireprasad002@gmail.com>
2026-03-26 00:00:47 +0530

    Fix mongoose connection syntax in test-create-user.js

--- Backend/test-create-user.js
+++ Backend/test-create-user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const User = require('./models/User');
 
-mongoose.connect('mongoose.connect(process.env.MONGO_URI)', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongoose.connect(process.env.MONGO_URI)')
   .then(async () => {
     // Clear old users if needed (optional)
     // await User.deleteMany({});
